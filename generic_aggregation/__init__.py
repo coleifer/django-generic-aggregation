b25bc8fb5c7e24b471d13fe6a995c2ec8efe31ca
Charles Leifer <coleifer@gmail.com>
2010-06-30 18:14:29 -0500

    Adding setup.py for use with pip

--- generic_aggregation/__init__.py
+++ generic_aggregation/__init__.py
@@ -1,1 +1,3 @@
 from generic_aggregation.utils import generic_aggregate, generic_annotate
+
+VERSION = (0, 1, 0)
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+from news import VERSION
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
+readme = f.read()
+f.close()
+
+setup(
+    name='django-generic-aggregation',
+    version=".".join(map(str, VERSION)),
+    description='annotate() and aggregate() for generically related data',
+    long_description=readme,
+    author='Charles Leifer',
+    author_email='coleifer@gmail.com',
+    url='http://github.com/coleifer/django-generic-aggregation/',
+    packages=find_packages(),
+    package_data = {
+        'generic_aggregation': [
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)

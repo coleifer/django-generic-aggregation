39537e9839efda74878138ff776b54f7ed39adfc
Charles Leifer <coleifer@gmail.com>
2010-07-27 10:34:59 -0500

    Fixing a bungled setup.py copy/paste foul

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-from news import VERSION
+from generic_aggregation import VERSION
 
 f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
 readme = f.read()
